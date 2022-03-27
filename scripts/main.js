be24e493ca74f8999bc366595b5fd0f4a781c73e
OlafZiorko <95439094+OlafZiorko@users.noreply.github.com>
2022-03-27 20:48:43 +0200

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 const burgerMenu = document.querySelector(".burger__menu");
 const mobileMenu = document.querySelector(".drop__down--menu");
-const closeMenu = document.querySelector(".close__button");
+const closeMenu = document.querySelector(".close__btn");
 
 burgerMenu.addEventListener("click", () => {
   mobileMenu.classList.add("visible");
